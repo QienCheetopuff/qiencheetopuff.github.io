fed44b9b6cef46ff5fed3434b0a438ed3026800f
Qien Ybarra <qientheafroman@gmail.com>
2023-09-02 19:40:07 -0500

    Update posttesting.js

---
+++ posttesting.js
@@ -0,0 +1,23 @@
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul");
+
+async function fetchPosts() {
+  try {
+    const responseData = await axios.get("/posts");
+    const listOfPosts = responseData;
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector("h2").textContent = post.title.toUpperCase();
+      postEl.querySelector("p").textContent = post.body;
+      postEl.querySelector("li").id = post.id;
+      listElement.append(postEl);
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+fetchButton.addEventListener("click", fetchPosts);
